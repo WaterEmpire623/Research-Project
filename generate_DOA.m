25bbab2aa34de98302dd1fe379dfdeeaf0094a59
WaterEmpire623 <97278754+WaterEmpire623@users.noreply.github.com>
2025-12-10 16:37:59 +0800

    Add files via upload
    update code based on my thoughts

--- generate_DOA.m
+++ generate_DOA.m
@@ -2,15 +2,18 @@ function [beta,phi,theta] =  generate_DOA(para)
 L  = para.path_num;
 K  = para.user_num;
 
-beta  = zeros(L,K); % path gains
-phi   = zeros(L,K); % BS path DoA-azimuth
-theta = zeros(L,K); % BS path DoA-elevation
+beta = normrnd(0,1,L,K) + 1i * normrnd(0,1,L,K); % path gains
+phi = unifrnd(-1,1,L,K); % BS path DoA-azimuth
+theta = unifrnd(-1,1,L,K); % BS path DoA-elevation
 
-for i=1:K
-    beta(:,:,i)  = normrnd(0,1,L,K) + 1i * normrnd(0,1,L,K); 
-    phi(:,:,i)   = unifrnd(-1,1,L,K); 
-    theta(:,:,i) = unifrnd(-1,1,L,K); 
-end
-
+% beta  = zeros(L,K); % path gains
+% phi   = zeros(L,K); % BS path DoA-azimuth
+% theta = zeros(L,K); % BS path DoA-elevation
+% 
+% for i=1:K
+%     beta(:,:,i)  = normrnd(0,1,L,K) + 1i * normrnd(0,1,L,K); 
+%     phi(:,:,i)   = unifrnd(-1,1,L,K); 
+%     theta(:,:,i) = unifrnd(-1,1,L,K); 
+% end
 
 end
