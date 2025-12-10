dc6b6177ba31700ae00037caa46bbd42b091313c
WaterEmpire623 <97278754+WaterEmpire623@users.noreply.github.com>
2025-12-10 16:37:16 +0800

    Add files via upload
    update main.m based on my thoughts

--- main.m
+++ main.m
@@ -8,6 +8,8 @@ Movable_region = [9,16,25,36,49,64,81,100]; % available region for antenna move
 
 CASE = Movable_region;
 XX = length(CASE);
+L  = para.path_num;
+K  = para.user_num;
 %% Determination
 all_rate  = zeros(para.monte_carlo,XX);
 mean_rate = zeros(1,XX);
@@ -16,8 +18,12 @@ for mon = 1:para.monte_carlo
     % disp(mon);
     [beta,phi,theta] = generate_DOA(para); % virtual DoAs of l path of the k user channel
     for cse = 1:XX
-        [H] = dictionary_channel(para,beta,phi,theta,CASE(cse));
-        %---------- you should write your algorithm here ----------
+        [h] = dictionary_channel(para,beta,phi,theta,CASE(cse));
+        H=zeros(CASE(cse),K);
+        for k = 1:K
+            H(:,k) = dictionary_channel(para,beta(:,k),phi(:,k),theta(:,k),CASE(cse));
+        end
+        % ---------- you should write your algorithm here ----------
         
     end
 end
@@ -25,6 +31,4 @@ end
 for i = 1:XX
     mean_rate(i) = mean(all_rate(:,i));      
 end
-
 %% Plot
-
