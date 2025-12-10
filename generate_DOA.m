0750630165584196e73222348d5d0951950269d5
WaterEmpire623 <97278754+WaterEmpire623@users.noreply.github.com>
2025-12-10 16:13:44 +0800

    Update generate_DOA.m
    discard changes made

--- generate_DOA.m
+++ generate_DOA.m
@@ -2,10 +2,6 @@ function [beta,phi,theta] =  generate_DOA(para)
 L  = para.path_num;
 K  = para.user_num;
 
-% beta = normrnd(0,1,L,K) + 1i * normrnd(0,1,L,K); % path gains
-% phi = unifrnd(-1,1,L,K); % BS path DoA-azimuth
-% theta = unifrnd(-1,1,L,K); % BS path DoA-elevation
-
 beta  = zeros(L,K); % path gains
 phi   = zeros(L,K); % BS path DoA-azimuth
 theta = zeros(L,K); % BS path DoA-elevation
@@ -15,5 +11,6 @@ for i=1:K
     phi(:,:,i)   = unifrnd(-1,1,L,K); 
     theta(:,:,i) = unifrnd(-1,1,L,K); 
 end
+
 
 end
