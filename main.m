537bb8cae3f42fd4641a4dd9c8d0f67362d3f476
WaterEmpire623 <97278754+WaterEmpire623@users.noreply.github.com>
2025-12-13 21:30:09 +0800

    Add files via upload
    12/13 update 1

--- main.m
+++ main.m
@@ -11,6 +11,9 @@ XX = length(CASE);
 L  = para.path_num;
 K  = para.user_num;
 %% Determination
+SINR = zeros(K);
+Signal = zeros(K);
+Noise = zeros(K);
 all_rate  = zeros(para.monte_carlo,XX);
 mean_rate = zeros(1,XX);
 %% Main
@@ -23,19 +26,28 @@ for mon = 1:para.monte_carlo
         for k = 1:K
             H_transpose(:,k) = dictionary_channel(para,beta(:,k),phi(:,k),theta(:,k),CASE(cse));
         end
-        H = transpose(H_transpose);
+        H = H_transpose';
         % ---------- PSO Algorithm ----------
-
-
-
-        % ---------- GA Algorithm ---------- 
-
-        
-        
+        F = zeros(CASE(cse),K);
+        objective_func = (norm(eye(K) - H*F,"fro"))^2 + para.alpha * (norm(F,"fro"))^2;
+        nvars = CASE(cse)*K;
+        F = particleswarm(objective_func, nvars);
+        % ---------- SINR ----------
+        for k = 1:K
+            Signal(k) = (H_transpose(:,k)*F(:,k))^2;
+            for j = 1:K
+                if(j ~= k)
+                    Noise(k) = Noise(j) + (H_transpose(:,k)*F(:,j))^2;
+                end
+            end
+            Noise(k) = Noise(k) + para.sigma_2;
+            SINR(k) = Signal/Noise;
+            all_rate(mon,cse) = all_rate(mon,cse) + log2(1+SINR(k));
+        end
     end
 end
 %% Average rate
 for i = 1:XX
-    mean_rate(i) = mean(all_rate(:,i));      
+    mean_rate(i) = mean(all_rate(:,i));   
 end
 %% Plot
