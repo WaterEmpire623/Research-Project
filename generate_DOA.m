f948c1fc2510f3ca17fbe67291c0b4ea2814177d
WaterEmpire623 <97278754+WaterEmpire623@users.noreply.github.com>
2025-12-16 00:11:38 +0800

    Add files via upload
    removed redundant annotation

--- generate_DOA.m
+++ generate_DOA.m
@@ -6,14 +6,4 @@ beta = normrnd(0,1,L,K) + 1i * normrnd(0,1,L,K); % path gains
 phi = unifrnd(-1,1,L,K); % BS path DoA-azimuth
 theta = unifrnd(-1,1,L,K); % BS path DoA-elevation
 
-% beta  = zeros(L,K); % path gains
-% phi   = zeros(L,K); % BS path DoA-azimuth
-% theta = zeros(L,K); % BS path DoA-elevation
-% 
-% for i=1:K
-%     beta(:,:,i)  = normrnd(0,1,L,K) + 1i * normrnd(0,1,L,K); 
-%     phi(:,:,i)   = unifrnd(-1,1,L,K); 
-%     theta(:,:,i) = unifrnd(-1,1,L,K); 
-% end
-
 end
